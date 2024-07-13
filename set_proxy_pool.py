2685af4ee4be8688b1569a364b90b2ca4c6aea6f
PURE <mintgreen@gmail.com>
2024-07-13 21:55:40 +0800

    新增多上游支持

--- set_proxy_pool.py
+++ set_proxy_pool.py
@@ -1,7 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-def set_proxy_pool(proxy_pool_url, api_url, auth):
+
+def set_proxy_pool(proxy_pool_url, api_url_list, auth):
     # auto-proxy-pool配置
     data = {
         "log": {},
@@ -10,25 +11,7 @@ def set_proxy_pool(proxy_pool_url, api_url, auth):
             "httpUseProxy": "false"
         },
         "users": {},
-        "upstream": {
-            "proxy": {
-                "apiUrl": api_url,
-                "upstreamFixedAuth": "",
-                "requestInterval": "1000ms",
-                "lifecycle": "200s",
-                "proactive": "50s",
-                "proactiveOnIdleSize": 0,
-                "proactiveOnIdleCheckInterval": "",
-                "maxSize": 3,
-                "loadBalanceMultiple": 0,
-                "disableOptimizationIp": False,
-                "enableReduceReuseIp": False,
-                "groupIndex": 0,
-                "failSleep": "",
-                "failThreshold": 0,
-                "disableHttpUseTunnel": False
-            }
-        },
+        "upstream": {},
         "changeRequest": [
             {
                 "hostRegex": ".*",
@@ -40,6 +23,27 @@ def set_proxy_pool(proxy_pool_url, api_url, auth):
         "dev": {},
         "DefaultFailBack": ""
     }
+
+    # 动态生成 upstream 配置
+    for index, api_url in enumerate(api_url_list):
+        proxy_name = f"proxy{index + 1}"
+        data["upstream"][proxy_name] = {
+            "apiUrl": api_url,
+            "upstreamFixedAuth": "",
+            "requestInterval": "1000ms",
+            "lifecycle": "50s",
+            "proactive": "20s",
+            "proactiveOnIdleSize": 0,
+            "proactiveOnIdleCheckInterval": "",
+            "maxSize": 3,
+            "loadBalanceMultiple": 0,
+            "disableOptimizationIp": False,
+            "enableReduceReuseIp": False,
+            "groupIndex": 0,
+            "failSleep": "",
+            "failThreshold": 0,
+            "disableHttpUseTunnel": False
+        }
 
     # 认证凭证
     auth = HTTPBasicAuth(auth['username'], auth['password'])
@@ -55,6 +59,7 @@ def set_proxy_pool(proxy_pool_url, api_url, auth):
     else:
         print(f"失败，状态码: {response.status_code}")
         return False
+
 
 if __name__ == '__main__':
     # content = set_proxy_pool('http://ip:prot', 'http://v2.api.juliangip.com/dynamic/getips?num=1&pt=1&result_type=text&split=2&trade_no=1xxxxxxxxxxx1&sign=exxxxxxxxxxxxxxxxx0', 'user', 'passwd')
